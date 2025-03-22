08c72253c1d4e1fe2b7104e4a786d70ec1b10588
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-03-22 13:14:18 +0100

    Add localization module

---
+++ Kliko's modloader/modules/localization/localizer.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import json
+
+
+METADATA: dict = {
+    "default": "en_US",
+    "available": {
+        "en_US": "English (United States)"
+    }
+}
+
+
+class Localizer:
+    DIRECTORY: Path = Path(__file__).parent / "strings"
+    language: str
+    strings: dict
+
+
+    @classmethod
+    def initialize(cls, language: str = METADATA["default"]) -> None:
+        if language not in METADATA["available"]: raise ValueError(f"Language '{language}' unavailable! Available lanaguages are: {', '.join(METADATA['available'].keys())}")
+
+        cls.language = language
+
+        default_filepath: Path = cls.DIRECTORY / f"{METADATA['default']}.json"
+        filepath: Path = cls.DIRECTORY / f"{language}.json"
+
+        cls.strings = cls._load_language(default_filepath)
+
+        if language != METADATA["default"]:
+            cls.strings.update(cls._load_language(filepath))
+
+
+    @classmethod
+    def _load_language(cls, filepath: Path) -> dict:
+        with open(filepath, "r", encoding="utf-8") as file:
+            data: dict = json.load(file)
+        return data

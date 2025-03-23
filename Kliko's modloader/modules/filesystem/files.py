2c4315aa0f596b559866f980a8854feb5e051c04
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-03-23 09:53:38 +0100

    Update filesystem module

--- Kliko's modloader/modules/filesystem/files.py
+++ Kliko's modloader/modules/filesystem/files.py
@@ -6,5 +6,5 @@ from .directories import Directory
 class File:
     """Dataclass storing the path objects to important files."""
 
+    CONFIG: Path = Directory.CONFIG / "config.json"
     MODS_CONFIG: Path = Directory.CONFIG / "mods.json"
-    STATE: Path = Directory.CONFIG / "config.json"

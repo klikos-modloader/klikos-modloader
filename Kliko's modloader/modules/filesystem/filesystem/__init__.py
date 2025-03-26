5069853cc7061f1a158d33c10b15ba28a1cf8b3c
TheKliko <121750609+TheKliko@users.noreply.github.com>
2025-03-26 13:17:56 +0100

    Delete Kliko's modloader/modules/filesystem/filesystem directory
    I'm stupid and add it to the wrong path

--- Kliko's modloader/modules/filesystem/filesystem/__init__.py
+++
@@ -1,6 +0,0 @@
-from .directories import Directory
-from .files import File
-from .open import open
-from .extract import extract
-from .download_stream_handler import StreamedDownloadHandler
-from .exceptions import *
